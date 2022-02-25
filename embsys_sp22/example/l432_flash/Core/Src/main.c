e1c9e02d406a8db229b516ebfe3bd359d1f20a96
Bryce Himebaugh <bhimebau@indiana.edu>
2022-02-25 10:44:44 -0500

    pre scaled ADC to correct L412 issue

--- embsys_sp22/example/l432_flash/Core/Src/main.c
+++ embsys_sp22/example/l432_flash/Core/Src/main.c
@@ -266,7 +266,7 @@ static void MX_ADC1_Init(void)
   /** Common config
   */
   hadc1.Instance = ADC1;
-  hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV256;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
   hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
